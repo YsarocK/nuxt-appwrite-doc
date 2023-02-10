31913ce2c5893d0382a2d721f4e0e2ea8a1b7f2e
Etienne <emoureton@gmail.com>
2023-02-10 22:59:36 +0100

    update identity

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   docus: {
     title: 'Nuxt Appwrite',
     description: 'A seamless Appwrite integration for Nuxt 3. (unofficial)',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    image: '/appwrite-cover.png',
     socials: {
       twitter: 'MouretonEtienne',
       github: 'YsarocK/nuxt-appwrite',
@@ -12,7 +12,7 @@ export default defineAppConfig({
       exclude: []
     },
     header: {
-      logo: true,
+      logo: false,
       showLinkIcon: true,
       exclude: []
     },
@@ -29,6 +29,6 @@ export default defineAppConfig({
       repo: 'nuxt-appwrite-doc',
       branch: 'main',
       edit: true
-    }
+    },
   }
 })
